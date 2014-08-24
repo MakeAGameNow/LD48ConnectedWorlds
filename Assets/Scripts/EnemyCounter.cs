b1061493c862f509891bed1baba082c0747dfbd4
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-08-24 16:55:07 -0700

    Enemy counter and prep for color stuff

---
+++ Assets/Scripts/EnemyCounter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCounter : MonoBehaviour
+{
+	[HideInInspector]
+	public int enemyCount;
+
+	// Update is called once per frame
+	void Update()
+	{
+		enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(enemyCount.ToString());
+	}
+}
