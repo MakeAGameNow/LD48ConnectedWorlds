a1a3dd351b7dc31aedc41b4c87476bc3b6b29575
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-08-24 04:42:33 -0700

    Setting up for cloud build and more graphic tweaking

---
+++ Assets/Scripts/TextureScroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScroller : MonoBehaviour
+{
+	[System.Serializable]
+	public class MaterialScrollInfo
+	{
+		public Material material;
+		public Vector2 scrollSpeed = Vector3.one;
+		public string textureName = "_MainTex";
+	}
+	public MaterialScrollInfo[] materialInfos;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach(MaterialScrollInfo materialInfo in materialInfos)
+		{
+			Vector2 oldPosition = materialInfo.material.GetTextureOffset(materialInfo.textureName);
+			materialInfo.material.SetTextureOffset(materialInfo.textureName,oldPosition + materialInfo.scrollSpeed*Time.deltaTime);
+		}
+	}
+}
