6e3331c6e9d6a0694d26ff9f5bf743ed2680716e
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-08-23 19:29:21 -0700

    Lots more experimentation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float height = 2.0f;
+	
+	void Update ()
+	{
+		transform.position += (transform.forward*Input.GetAxis("Vertical")+transform.right*Input.GetAxis("Horizontal"))*speed*Time.deltaTime;
+		//rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical")*thrustForce), ForceMode.Acceleration);
+	}
+
+	void LateUpdate()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position-transform.up*height*2.0f,transform.up,out hit))
+		{
+			transform.position = hit.point+transform.up*height;
+			Vector3 newUp = hit.transform.position-transform.position;
+			transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.right,newUp),newUp);
+		}
+	}
+}
