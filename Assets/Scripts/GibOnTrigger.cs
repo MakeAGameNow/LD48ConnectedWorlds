fe77b3bca075c7dddb3398936519cdcb40fcdd25
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-08-24 17:20:59 -0700

    Basic shooting

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour {
+
+	void OnTriggerEnter()
+	{
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/SphereOrienter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereOrienter : MonoBehaviour
+{
+	public float height = 2.0f;
+	public LayerMask layerMask = -1;
+
+	void LateUpdate()
+	{
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position-transform.up*height*2.0f,transform.up,out hit, Mathf.Infinity, layerMask))
+		{
+			transform.position = hit.point+transform.up*height;
+			Vector3 newUp = hit.transform.position-transform.position;
+			transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.right,newUp),newUp);
+		}
+	}
+}
