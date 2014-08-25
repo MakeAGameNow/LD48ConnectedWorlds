1c8c403256b8f8c9250400579324c75cafcabfc3
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-08-24 17:40:23 -0700

    Tweaks and stuff

---
+++ Assets/Scripts/DestroyParticleSystemAfterTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticleSystemAfterTime : MonoBehaviour
+{
+	public float time = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke("DestroyParticle", time);
+	}
+
+	void Update()
+	{
+		if(!particleSystem.IsAlive())
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	void DestroyParticle()
+	{
+		particleSystem.Stop();
+		collider.enabled = false;
+	}
+}
