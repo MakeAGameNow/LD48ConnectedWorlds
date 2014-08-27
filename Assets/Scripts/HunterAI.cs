b118bd017ec6146a8d51bdb0e5e9f23ab2271651
Maximilian Maroe <max.maroe@siliconbeachmedia.com>
2014-08-27 13:11:38 -0700

    AutoPlay

---
+++ Assets/Scripts/HunterAI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HunterAI : MonoBehaviour
+{
+	public float rotationSpeed = 20.0f;
+	public float shootAngle = 10.0f;
+
+	private GameObject[] enemies;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+		float closest = Mathf.Infinity;
+		GameObject closestObj = null;
+
+		foreach(GameObject enemy in enemies)
+		{
+			Vector3 direction = enemy.transform.position-transform.position;
+
+			float distance = direction.sqrMagnitude;
+
+			if(distance < closest && Vector3.Dot(transform.forward,direction) > 0.0f)
+			{
+				closest = distance;
+				closestObj = enemy;
+			}
+		}
+
+		if(closestObj != null)
+		{
+
+			Quaternion previousRotation = transform.rotation;
+
+			Quaternion goalRotation = Quaternion.LookRotation(closestObj.transform.position-transform.position,transform.up);
+
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, goalRotation, rotationSpeed*Time.deltaTime);
+
+			if(Quaternion.Angle(goalRotation,transform.rotation) < shootAngle)
+			{
+				Shoot shooter = GetComponentInChildren<Shoot>();
+				shooter.shootNow = true;
+			}
+		}
+	}
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class Shoot : MonoBehaviour
 {
 	public GameObject bullet;
+	public float shootSpeed = 0.5f;
+	public bool shootNow = false;
+	public bool canShoot = true;
+
+	void Start()
+	{
+	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if(Input.GetButtonDown("Fire1"))
+		if((Input.GetButtonDown("Fire1") || shootNow) && canShoot)
 		{
 			Instantiate(bullet,transform.position,transform.rotation);
+			canShoot = false;
+			Invoke("ResetShoot",shootSpeed);
 		}
+		shootNow = false;
+	}
+
+	void ResetShoot()
+	{
+		canShoot = true;
 	}
 }
